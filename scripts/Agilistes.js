eaa05bcc04f7e28af35b59d0a9d04d35d476ded9
Benoit Lemoine <blemoine@xebia.fr>
2014-07-04 12:48:02 +0200

    Integration text et image que j'ai

--- scripts/Agilistes.js
+++ scripts/Agilistes.js
@@ -1,3 +1,42 @@
+window.AGILISTE_MAIN = [
+    {
+        title: 'Mon backlog ne ressemble à rien',
+        text: "La jachère a des vertus agricoles indéniables et vous promet des récoltes fructueuses si vous avez la patience d'attendre quelques années. Pas sûr en revanche que ce soit la meilleure stratégie pour faire fructifier votre Backlog. Adopter ce coach pour raser les mauvaises herbes et planter des graines de stories prometteuses.",
+        name: 'Emmanuel Sciara',
+        image_url: 'emmanuel_sciara.jpg'
+    },
+    {
+        title: 'Je livre toujours ... En retard',
+        text: "Vous livrez toujours en retard? Le manque de prédictibilité n'est pas une fatalité. Ce coach est équipé de tous les outils nécessaires pour vous aider : tee-shirts de tailles variées, mètre étalon de Fibonacci, capteurs de flux, générateur de statistiques. Avec lui dans votre caddie, vous livrerez à l'heure!",
+        name: 'Gwenael Bonhommeau',
+        image_url: 'gwenael_bonhommeau.jpg'
+    },
+    {
+        title: "Mon projet c'est la jungle",
+        text: "Depuis que le tigre Shere Khan a repris la direction de projet, le Scrum Master Mowgli a bien du mal à vivre au milieu des jours-hommes, surtout que l'architecte Kaa tente de l'hypnotiser à chaque rétrospective. La bienveillance du PO Baloo ne sera pas suffisante pour trouver le chemin du village de la release, adoptez Baggera pour tracer votre route en terrain hostile.",
+        name: 'Renaud Chevalier',
+        image_url: 'renaud_chevalier.jpg'
+    },
+    {
+        title: 'On livre le 12 ... mais de quel mois??',
+        text: "Ca fait 1 an et demi qu'on livre à la fin du mois? Pas de problème, c'est totalement IN le ?????, vous êtes encore dans le coup. Sinon vous pouvez adopter ce petit bijou. Posez le au milieu de vos équipes et il mettra votre Time To Market au régime.",
+        name: 'Gilles Mantel',
+        image_url: 'gilles_mantel.jpg'
+    },
+    {
+        title: 'Je suis un génie incompris ... de ses utilisateurs',
+        text: "Le PO est-il brillant ou complètement à la rue ? C'est le genre de discussions qui animent vos équipes à la machine à café. Et franchement vous êtes bien incapable de trancher le débat tellement il est difficile de comprendre ses arguments. Il est temps d'adopter la fille spirituelle de Steve Jobs pour sauver votre produit de l'échec qui l'attend.",
+        name: 'Laurène Vol',
+        image_url: 'laurene_vol.jpg'
+    },
+    {
+        title: "J'ai plus de bugs que de stories",
+        text: "Dératisation, contrôle des termittes, vérification de l'humidité, bilan thermique, … Heureusement que les normes dans le logiciel sont moins drastiques que dans le bâtiment, car vous auriez peu de chance de revendre votre logiciel. Sinon optez pour un bilan complet avec notre expert.",
+        name: 'Clément Rochas',
+        image_url: 'clement_rochas.jpg'
+    }
+];
+
 window.AGILISTE_MINIATURE = [
     {
         image_url: 'benjamin_moitie_mini.png',
@@ -49,58 +88,5 @@ window.AGILISTE_MINIATURE = [
         title: 'Kanban Experte',
         description: 'La pro du post-it'
     }
-
-];
-
-window.AGILISTE_MAIN = [
-    {
-        title: 'Je livre toujours en retard',
-        text: "La mission de Xebia est de développer du logiciel de très haute qualité (Software Development Done" +
-            "Right)." +
-            "L'ambition de notre équipe agile est de changer, dans le paysage de l'IT en France," +
-            "les pratiques peu efficaces et une culture tayloriste du développement logiciel. Nous vous proposons" +
-            "une" +
-            "offre d'accompagnement riche et complète, depuis l'idée jusqu'à la production. Notre offre vous" +
-            "permettra de" +
-            "tirer" +
-            "le meilleur de vos équipes en les associant étroitement à notre démarche. Nous partageons également" +
-            "généreusement notre" +
-            "savoir-faire au travers de notre blog, de publications, ou participations aux conférences.'",
-        name: 'Gwenael Bonhommeau',
-        image_url: 'gweneal_bonhommeau.JPG'
-    },
-    {
-        title: 'Je livre toujours en retard 2',
-        text: "La mission de Xebia est de développer du logiciel de très haute qualité (Software Development Done" +
-            "Right)." +
-            "L'ambition de notre équipe agile est de changer, dans le paysage de l'IT en France," +
-            "les pratiques peu efficaces et une culture tayloriste du développement logiciel. Nous vous proposons" +
-            "une" +
-            "offre d'accompagnement riche et complète, depuis l'idée jusqu'à la production. Notre offre vous" +
-            "permettra de" +
-            "tirer" +
-            "le meilleur de vos équipes en les associant étroitement à notre démarche. Nous partageons également" +
-            "généreusement notre" +
-            "savoir-faire au travers de notre blog, de publications, ou participations aux conférences.'",
-        name: 'Gwenael Bonhommeau',
-        image_url: 'gweneal_bonhommeau.JPG'
-    },
-    {
-        title: 'Je livre toujours en retard 3',
-        text: "La mission de Xebia est de développer du logiciel de très haute qualité (Software Development Done" +
-            "Right)." +
-            "L'ambition de notre équipe agile est de changer, dans le paysage de l'IT en France," +
-            "les pratiques peu efficaces et une culture tayloriste du développement logiciel. Nous vous proposons" +
-            "une" +
-            "offre d'accompagnement riche et complète, depuis l'idée jusqu'à la production. Notre offre vous" +
-            "permettra de" +
-            "tirer" +
-            "le meilleur de vos équipes en les associant étroitement à notre démarche. Nous partageons également" +
-            "généreusement notre" +
-            "savoir-faire au travers de notre blog, de publications, ou participations aux conférences.'",
-        name: 'Gwenael Bonhommeau',
-        image_url: 'gweneal_bonhommeau.JPG'
-    }
-
 
 ];
